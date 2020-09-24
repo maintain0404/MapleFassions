24be3e8544524da9b2b701e45b224b0a0711b7b0
Taein Min <maintain0404@gmail.com>
2020-09-24 18:49:47 +0900

    HSI_classifier implement finished - json settings moved to settings.py

--- classifier.py
+++ classifier.py
@@ -1,8 +1,10 @@
 import numpy as np
 import json
 from collections import OrderedDict, Counter
+from settings import COLOR_CODE, COLOR_HUE
 
-COLORS = json.load(open('rgb_colors.json', 'r'))
+RGB_COLORS = json.load(open('rgb_colors.json', 'r'))
+HSI_COLORS = json.load(open('hsi_colors.json', 'r'))
 
 def rgb_classify(img):
     color_distances = {}
@@ -34,6 +36,15 @@ def rgb_classify(img):
     ## 투명한 곳이 흰색으로 판정되는 문제가 있는듯
     ## 또한 없는 색이 컬러 인덱스에서 누락되어 잘못된 판정이 있을 수 있음
 
+def hue_distance(a, b):
+    dstnc = abs(a - b)
+    return dstnc if dstnc < 180 else abs(360 - dstnc)
+
+def define_hue_color(hue_value):
+    for k, v in HSI_COLORS.items():
+        if hue_distance(hue_value, v) <= 15:
+            return k
+    
 def hsi_classify(img):
     color_distances = []
     R, G, B, A = (img[:,:,0].astype(np.float128), img[:,:,1].astype(np.float128),
@@ -44,20 +55,40 @@ def hsi_classify(img):
 
     # 색상 구하기
     hue_numerator = ((R - G) + (R - B)) * 0.5
-    print(hue_numerator)
     hue_denominator = np.sqrt(np.square(R - G) + (R - B) * (G - B))
-    print(hue_denominator)
-    print(hue_numerator / hue_denominator)
     hue = np.rad2deg(np.arccos(hue_numerator / hue_denominator))
     idx_to_reverse = np.where(img[:,:,2] > img[:,:,1])
-    x, y = idx_to_reverse
-    for idx in range(len(idx_to_reverse[0])):
-        hue[x[idx], y[idx]] = 360 - hue[x[idx], y[idx]]
+    for x, y in zip(idx_to_reverse[0], idx_to_reverse[1]):
+        hue[x, y] = 360 - hue[x, y]
+    # x, y = idx_to_reverse
+    # for idx in range(len(idx_to_reverse[0])):
+    #     hue[x[idx], y[idx]] = 360 - hue[x[idx], y[idx]]
 
     # 명도 구하기
     intensity = (R + G + B) / 3
 
     # hsia
-    hsia = np.array([hue, saturation, intensity, A])   
+    hsia = np.array([hue, saturation, intensity, A])
+    
+    nans = np.where(np.isnan(hue) == True)
+    # nan지점을 -30 초기화
+    for x, y in zip(nans[0], nans[1]):
+        hue[x, y] = -30
+    
+    # 색상코드로 변환
+    result = np.add(np.rint(np.divide(hue, 30)), 1)
+    reds = np.where(result == 13)
+    for x, y in zip(reds[0], reds[1]):
+        result[x, y] = 1
 
-    return hsia
+    # 무채색 분리
+    achromatic = np.where(saturation < 0.1)
+    for x, y in zip(achromatic[0], achromatic[1]):
+        if intensity[x, y] > 192:
+            result[x, y] = COLOR_CODE.WHITE
+        elif intensity[x, y] < 64:
+            result[x, y] = COLOR_CODE.BLACK
+        else:
+            result[x, y] = COLOR_CODE.GRAY
+
+    return Counter(np.ravel(result))
---
+++ settings.py
@@ -0,0 +1,49 @@
+class COLOR_CODE:
+    TRANSPARENT = 0
+    RED = 1
+    ORANGE = 2
+    YELLOW = 3
+    YELLOWGREEN = 4
+    GREEN = 5
+    TURQUOISE = 6
+    CYAN = 7
+    COBALT = 8
+    BLUE = 9
+    PURPLE = 10
+    MARGENTA = 11
+    ROSE = 12
+    BLACK = 13
+    WHITE = 14
+    GRAY = 15
+
+class COLOR_RGB:
+    BLACK = [0, 0, 0]
+    WHITE = [255, 255, 255]
+    GRAY = [128, 128, 128]
+    RED = [255, 0, 0]
+    GREEN = [0, 255, 0]
+    BLUE = [0, 0, 255]
+    YELLOW = [255, 255, 0]
+    PURPLE = [0, 128, 128]
+    SKYBLUE = [135, 206, 235]
+    YELLOWGREEN = [173, 255, 47]
+    PINK = [255, 192, 203]
+    BROWN = [139, 69, 19]
+    ORANGE = [255, 140, 0]
+    NAVY = [0, 0, 128]
+    VIOLET = [238, 130, 238]A
+
+class COLOR_HUE:
+    RED = 0
+    ORANGE = 30
+    YELLOW = 60
+    YELLOWGREEN = 90
+    GREEN = 120
+    TURQUOISE = 150
+    CYAN = 180
+    COBALT = 210
+    BLUE = 240
+    PURPLE = 270
+    MARGENTA = 300
+    ROSE = 330
+    
